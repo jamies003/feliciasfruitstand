12066ed257271c17ceabae364ea730310fd0d7f9
Jamie Stewart <stwjme@gmail.com>
2020-04-07 19:13:48 -0400

    for loops

--- fruitsales.js
+++ fruitsales.js
@@ -1,4 +1,8 @@
 let fruitInventory = ["Apples", "Peaces", "Pumpkins", "Endives" ];
 let fruitPrices = [1, 1.5, 7, 12]
 
+for (i = 0 ; i < fruitInventory.length; i++){
+    console.log(` ${fruitInventory[i]}`)
+} 
+
 
